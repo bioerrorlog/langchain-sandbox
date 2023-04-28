e1dec432465a2d21abf899c70e1344aae0c8107c
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-04-28 17:52:42 +0900

    chat prompt template

--- quickstart/main.py
+++ quickstart/main.py
@@ -1,12 +1,23 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage,
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
 )
 
 chat = ChatOpenAI(temperature=0)
 
-response = chat([HumanMessage(content="Translate this sentence from English to French. I love programming.")])
-print(response)
-print(type(response))
+template = "You are a helpful assistant that translates {input_language} to {output_language}."
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template = "{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+# get a chat completion from the formatted messages
+response = chat(chat_prompt.format_prompt(input_language="English",
+                                          output_language="French", text="I love programming.").to_messages())
+# -> AIMessage(content="J'aime programmer.", additional_kwargs={})
+
+print(response)  # content="J'aime programmer." additional_kwargs={}
+print(type(response))  # <class 'langchain.schema.AIMessage'>
