b64494540f61bba4fb07251e955accc2228822a0
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-04-29 12:31:39 +0900

    init agents example

--- quickstart/main.py
+++ quickstart/main.py
@@ -1,23 +1,17 @@
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
+
 
 chat = ChatOpenAI(temperature=0)
+tools = load_tools(["terminal"])
 
-template = "You are a helpful assistant that translates {input_language} to {output_language}."
-system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-human_template = "{text}"
-human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+agent = initialize_agent(
+    tools,
+    chat,
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
 
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-# get a chat completion from the formatted messages
-response = chat(chat_prompt.format_prompt(input_language="English",
-                                          output_language="French", text="I love programming.").to_messages())
-# -> AIMessage(content="J'aime programmer.", additional_kwargs={})
-
-print(response)  # content="J'aime programmer." additional_kwargs={}
-print(type(response))  # <class 'langchain.schema.AIMessage'>
+agent.run("Who is the current terminal user?I would like to know the list of directories under that user's home directory.")
