cd125cd71d167acab80877a8d2e4d0c650939fed
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-05-08 22:32:59 +0900

    use chat model

--- indexes/main.py
+++ indexes/main.py
@@ -1,13 +1,17 @@
-# from langchain.chains import RetrievalQA
-# from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+# from langchain.text_splitter import TokenTextSplitter
 
 
-# loader = TextLoader('./data/hashire_merosu.txt', encoding='shift_jis')
-loader = TextLoader('./data/bocchan.txt', encoding='shift_jis')
+llm = ChatOpenAI(temperature=0)
+
+loader = TextLoader('./data/hashire_merosu.txt', encoding='shift_jis')
+# loader = TextLoader('./data/bocchan.txt', encoding='shift_jis')
 # loader = TextLoader('./data/melos.txt', encoding='utf8')
-index = VectorstoreIndexCreator().from_loaders([loader])
+index = VectorstoreIndexCreator(
+    # text_splitter=TokenTextSplitter()
+).from_loaders([loader])
 
-query = "メロスは誰ですか？"
-print(index.query(query))
+query = "メロスは間に合いましたか？"
+print(index.query(question=query, llm=llm))
